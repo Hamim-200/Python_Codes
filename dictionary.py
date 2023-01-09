34f52b0c1acd104622beaaf4d6ade56fc8283ef5
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-01-09 21:47:00 +0600

    dictonary added

--- dictionary.py
+++ dictionary.py
@@ -2,5 +2,10 @@ a = {'name': 'John', 'age': 30, 'city': 'New York'}
 b = {'hometown': 'Chandpur', 'fav_language': 'Python'}
 # a['PhoneNumber'] = '098765432123'
 # print(a['name'])
-a.update(b)
-print(a)
+# a.update(b)
+
+
+# print(a.get('gender', 'Not Avaiable'))
+print(a.items())
+print(a.keys())
+print(a.values())
