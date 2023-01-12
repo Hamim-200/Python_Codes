9a9d1d0a80ee2a20e60e25abebbd14dc399abf7f
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-01-12 16:58:30 +0600

    loop

---
+++ loop.py
@@ -0,0 +1,41 @@
+# initiazation
+# while logica_part:
+#     statement
+
+# n = 1
+# while n <= 10:
+#     print(n)
+#     n = n+1
+
+
+# 1+2+3+4+5....
+
+# n = 10
+# result = n*(n+1)/2
+# print(result)
+
+
+# n = 1
+# sum = 0
+# while n <= 5:
+#     sum = sum+n
+#     n = n+1
+# print(sum)
+
+
+# FOR LOOP
+a = ['cat', 'Dog', 'Bird', 'Lion', 'Tiger', 0, 0.2, 76]
+
+for item in a:
+    print(item)
+
+b = 'Python'
+lst = []
+
+for letter in b:
+    print(letter)
+    lst.append(letter)
+print(lst)
+
+for i in range(1, 11):
+    print(i)
