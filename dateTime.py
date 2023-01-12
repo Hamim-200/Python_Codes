392abe763bcb196bb0fc5741699cf9bba085d4ae
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-01-13 00:52:17 +0600

    date time

---
+++ dateTime.py
@@ -0,0 +1,13 @@
+# from datetime import date
+# temp = date.today()
+# # print(temp)
+
+# print(temp.month)
+# print(temp.year)
+
+from datetime import datetime
+temp = datetime.now()
+print(temp)
+print(temp.hour)
+print(temp.minute)
+print(temp.second)
